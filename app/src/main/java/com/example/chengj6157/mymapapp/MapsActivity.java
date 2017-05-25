784bdf44c1ea4a18818b85bf3ceb3661792ca158
juice-tin <j2011cheng@gmail.com>
2017-05-25 11:09:44 -0700

    Justin Cheng
    added the birthplace marker and added the current location button.
    working on its functioning

--- app/src/main/java/com/example/chengj6157/mymapapp/MapsActivity.java
+++ app/src/main/java/com/example/chengj6157/mymapapp/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.example.chengj6157.mymapapp;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,6 +32,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -39,8 +48,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng birth = new LatLng(32.798289, -117.155401);
+        mMap.addMarker(new MarkerOptions().position(birth).title("Born Here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
+    }
+
+    public void toCurrentLoc(GoogleMap googleMap) {
+        mMap = googleMap;
+        if ((ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.
+        ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) && ContextCompat.checkSelfPermission(
+                this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            LatLng currentLoc = ;
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            mMap.setMyLocationEnabled(false);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+
     }
 }
